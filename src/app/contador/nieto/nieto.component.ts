cf02211bba32a08f43c2c3f6af6cd7a919e75938
David Toledo <dtoledoz0207@gmail.com>
2020-05-24 13:11:12 -0500

    using-nietoComponent

--- src/app/contador/nieto/nieto.component.ts
+++ src/app/contador/nieto/nieto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nieto',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NietoComponent implements OnInit {
 
+  @Input() contador: number;
+  @Output() contadorCambio = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  reset() {
+    this.contador = 0;
+    this.contadorCambio.emit(this.contador);
   }
 
 }
