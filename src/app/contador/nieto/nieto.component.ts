6c93bbf29aa8c1ae55ce6815f425b668d8048664
David Toledo <dtoledoz0207@gmail.com>
2020-05-24 12:54:26 -0500

    create-hijo-nieto-components

---
+++ src/app/contador/nieto/nieto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nieto',
+  templateUrl: './nieto.component.html',
+  styles: []
+})
+export class NietoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
