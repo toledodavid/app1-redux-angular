2078f196f4f06bf38874e2a4a6dbd3075e66e28c
David Toledo <dtoledoz0207@gmail.com>
2020-05-25 12:04:18 -0500

    install-store-devtools-and-import-in-appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 // Ngrx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { contadorReducer } from './contador/contador.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -19,6 +21,10 @@ import { NietoComponent } from './contador/nieto/nieto.component';
   imports: [
     BrowserModule,
     StoreModule.forRoot({ contador: contadorReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
     AppRoutingModule
   ],
   providers: [],
