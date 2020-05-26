fa022bb5829edaaeec7e2647b3fd27b1592d9c60
David Toledo <dtoledoz0207@gmail.com>
2020-05-25 22:24:19 -0500

    create-multiplicar-dividir-actions

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,11 +1,13 @@
 import { createReducer, on } from '@ngrx/store';
-import { incrementar, decrementar } from './contador.actions';
+import { incrementar, decrementar, multiplicar, dividir } from './contador.actions';
 
 export const initialState = 20;
 
 const _contadorReducer = createReducer(initialState,
   on(incrementar, state => state + 1),
   on(decrementar, state => state - 1),
+  on(multiplicar, (state, {numero}) => state * numero),
+  on(dividir, (state, {numero}) => state / numero),
 );
 
 
